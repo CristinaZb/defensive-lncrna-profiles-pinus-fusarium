6d3d01de41fd5e0ae37adf7445c6dbb3e0cc7d46
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-03 12:12:10 +0100

    Update and rename fastqc_before.sh to fastqc.sh

--- scripts/fastqc.sh
+++ scripts/fastqc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-PATH_RAW="/dual_lncrna/raw_data" 
-PATH_OUT="/dual_lncrna/fastqc_before" 
 module load haswell/FastQC_0.11.9 
 # Run FastQC 
 for i in $(ls $PATH_RAW/*.fastq.gz) ; do 
