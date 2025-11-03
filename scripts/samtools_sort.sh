9d232bb3eff697cb73de316c2bb59debbdb56e3b
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-03 15:45:24 +0100

    Create samtools_sort.sh

---
+++ scripts/samtools_sort.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+file=$(ls *.bam | cut -f1 -d. | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
+
+module load haswell/samtools_1.7
+
+samtools sort -@ $SLURM_NTASKS -o ${file}.sorted.bam ${file}.fix.bam
