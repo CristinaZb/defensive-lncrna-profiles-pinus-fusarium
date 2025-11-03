453b09dcf1111a8158200f816ce3d26ebbc6e922
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-03 19:43:08 +0100

    Create 05_stringtie_merge.sh

---
+++ scripts/05_stringtie_merge.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ls -1 *.gtf >> sample_assembly_gtf_list.txt
+
+module load haswell/StringTie_2.1.4
+
+stringtie --merge -p $SLURM_NTASKS \
+-G $GTF/Pita.2_01.gtf \
+-F 0.5 sample_assembly_gtf_list.txt \
+-o ${spp}_transcripts.gtf \
+-l ${spp}
