898fad5ce1e3c3ae0c2e4ffec0ad927c7d5192f8
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:57:33 +0100

    Update 03_hisat2_pine.sh

--- scripts/03_hisat2_pine.sh
+++ scripts/03_hisat2_pine.sh
@@ -12,11 +12,11 @@ name3=${files_3[$SLURM_ARRAY_TASK_ID-1]}
 echo $SLURM_ARRAY_TASK_ID
 file=$(ls | cut -d_ -f1 | uniq | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 
-module load haswell/HISAT2_2.0.0
+module load HISAT2_2.0.0
 
 # Run Hisat2
 
 hisat2 -p $SLURM_NTASKS --rg-id=${file} --rg SM:${file} --rg PL:ILLUMINA \
 -x $PITA_PATH/genome_pita_index --dta --rna-strandness RF \
 -1 ${name1} -2 ${name2} -U ${name3} \
--S $MAPPED_PATH/${file}.sam
+-S ${file}.sam
