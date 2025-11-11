30c8b24f63da92ba0316c61f5744a283af4d9e1b
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:57:07 +0100

    Update 03_hisat2_fc.sh

--- scripts/03_hisat2_fc.sh
+++ scripts/03_hisat2_fc.sh
@@ -12,11 +12,11 @@ name3=${files_3[$SLURM_ARRAY_TASK_ID-1]}
 echo $SLURM_ARRAY_TASK_ID
 file=$(ls | cut -f1 -d_ | uniq | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 
-module load haswell/HISAT2_2.0.0
+module load HISAT2_2.0.0
 
 # Run Hisat2
 
 srun -n 1 hisat2 -p $SLURM_NTASKS --rg-id=${file} --rg SM:${file} --rg PL:ILLUMINA \
 -x $FCIRC_PATH/genome_fc072v_index --dta --rna-strandness RF \
 -1 ${name1} -2 ${name2} -U ${name3} \
--S $MAPPED_PATH/${file}-fc072v.sam
+-S ${file}-fc072v.sam
