0238bfbb52196fca1c4b22c7604f9d2dd824fbba
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:59:11 +0100

    Update 04_5_samtools_rmdup.sh

--- scripts/04_5_samtools_rmdup.sh
+++ scripts/04_5_samtools_rmdup.sh
@@ -2,6 +2,6 @@
 
 file=$(ls *.bam | cut -f1 -d. | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 
-module load haswell/samtools_1.7
+module load samtools_1.7
 
 samtools markdup -r -s -@ $SLURM_NTASKS ${file}.sorted.bam ${file}.dedup.bam
