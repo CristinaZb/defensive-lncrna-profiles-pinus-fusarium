0e025fe89958ab3fda2770e0df6522d9cdf8c763
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:59:35 +0100

    Update 05_1_stringtie_fc.sh

--- scripts/05_1_stringtie_fc.sh
+++ scripts/05_1_stringtie_fc.sh
@@ -2,6 +2,6 @@
 
 file=$(ls *.bam | cut -f1 -d. | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 
-module load haswell/StringTie_2.1.4
+module load StringTie_2.1.4
 
 stringtie ${file}.dedup.bam -p $SLURM_NTASKS -l ${file}_FCRIC --rf -o $OUT_PATH/${file}_transcripts.gtf
