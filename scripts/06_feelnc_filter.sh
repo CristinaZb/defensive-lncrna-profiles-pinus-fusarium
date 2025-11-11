d225b7a0ce140c30e2685a9ed14b163c98136e57
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 20:05:31 +0100

    Update 06_feelnc_filter.sh

--- scripts/06_feelnc_filter.sh
+++ scripts/06_feelnc_filter.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Activate conda environment
+source miniconda3/etc/profile.d/conda.sh
+conda activate feelnc_env
+
 FEELnc_filter.pl -p $SLURM_NTASKS -i unknown_${spp}.annotated.gtf -a Pita.2_01.gtf --monoex=-1 --biex=1 > candidate_${spp}-lncRNA.gtf
+
+# deactivate the environment
+conda deactivate
