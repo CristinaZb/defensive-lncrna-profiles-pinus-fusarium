d7c8338f7729ca49f23f95685c9e827e9b241f3d
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:58:52 +0100

    Update 04_4_samtools_sort.sh

--- scripts/04_4_samtools_sort.sh
+++ scripts/04_4_samtools_sort.sh
@@ -2,6 +2,6 @@
 
 file=$(ls *.bam | cut -f1 -d. | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 
-module load haswell/samtools_1.7
+module load samtools_1.7
 
 samtools sort -@ $SLURM_NTASKS -o ${file}.sorted.bam ${file}.fix.bam
