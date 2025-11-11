af0bad07ac8a5286d2f697d455c9ecda28955112
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 20:00:27 +0100

    Update 05_3_gffread_fc.sh

--- scripts/05_3_gffread_fc.sh
+++ scripts/05_3_gffread_fc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load haswell/gffread_0.12.1
+module load gffread_0.12.1
 
 # Run gffread utility
 gffread -w fc_transcripts.fa -g Fusarium_circinatum_FC072V.fa fc_transcripts.gtf
