6672f54e89f9836e073f0859f332f5b85e393731
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-11 19:56:39 +0100

    Update 02_trimming.sh

--- scripts/02_trimming.sh
+++ scripts/02_trimming.sh
@@ -12,6 +12,5 @@ file=$(ls | cut -f1 -d_ | uniq | tail -n +$SLURM_ARRAY_TASK_ID | head -n 1)
 # Run Trimmomatic
 
 java -jar $TRIMMOMATIC_PATH/trimmomatic-0.38.jar PE -threads $SLURM_NTASKS ${name1} ${name2} \
-$TRIMMOMATIC_OUTPUT/${file}_1p.fastq.gz $TRIMMOMATIC_OUTPUT/${file}_1u.fastq.gz \
-$TRIMMOMATIC_OUTPUT/${file}_2p.fastq.gz $TRIMMOMATIC_OUTPUT/${file}_2u.fastq.gz \
+${file}_1p.fastq.gz ${file}_1u.fastq.gz ${file}_2p.fastq.gz ${file}_2u.fastq.gz \
 ILLUMINACLIP:$TRIMMOMATIC_PATH/adapters/TruSeq3-PE.fa:2:30:10 HEADCROP:10
