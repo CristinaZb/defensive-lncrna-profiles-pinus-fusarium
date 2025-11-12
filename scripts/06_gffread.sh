d15e4c14615de091659553f7b55da5518ecdc2e7
CristinaZb <108120372+CristinaZb@users.noreply.github.com>
2025-11-12 11:29:13 +0100

    Update 06_gffread.sh

--- scripts/06_gffread.sh
+++ scripts/06_gffread.sh
@@ -4,4 +4,4 @@ module load gffread_0.12.1
 
 # Run gffread utility
 
-gffread -w candidate_${spp}-lncRNA.fa -g Pita.1_2.fa candidate_${spp}-lncRNA.gtf
+gffread -w candidate_${spp}-lncRNA.fa -g Pita.2_01.fa candidate_${spp}-lncRNA.gtf
